7b8f1acd98d04a06f95450c5a08498d4958ddb49
Soumen Ganguly <soumendotganguly@gmail.com>
2020-01-23 18:52:54 +0100

    Added random distribution

--- exploring_distributions.py
+++ exploring_distributions.py
@@ -1,5 +1,17 @@
 # Create a random distribution, uniform distribution and normal distribution 
-
+import random
 import matplotlib.pyplot as plt
+import numpy as np
+#import seaborn as sns
 
-n = 10,000
+# Random distribution
+list_of_rand_numbers = []
+for i in range(0,100):
+    list_of_rand_numbers.append(random.randint(90,110))
+
+plt.hist(list_of_rand_numbers, 21, density=1)
+plt.xlabel('Random numbers')
+plt.ylabel('Probability density')
+plt.title('Probability distribution of random numbers in the range 90-110')
+plt.show()
+
