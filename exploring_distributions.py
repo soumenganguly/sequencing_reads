91c5f385c9327c45ce88e88c91d646e76513f2e5
Soumen Ganguly <soumendotganguly@gmail.com>
2020-01-24 16:58:53 +0100

    Added uniform distribution

--- exploring_distributions.py
+++ exploring_distributions.py
@@ -1,17 +1,30 @@
 # Create a random distribution, uniform distribution and normal distribution 
-import random
-import matplotlib.pyplot as plt
 import numpy as np
-#import seaborn as sns
+from scipy.stats import uniform
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(rc={'figure.figsize':(10,10)})
 
 # Random distribution
 list_of_rand_numbers = []
 for i in range(0,100):
-    list_of_rand_numbers.append(random.randint(90,110))
+    list_of_rand_numbers.append(np.random.randint(90,110))
 
-plt.hist(list_of_rand_numbers, 21, density=1)
-plt.xlabel('Random numbers')
-plt.ylabel('Probability density')
-plt.title('Probability distribution of random numbers in the range 90-110')
+ax = sns.distplot(list_of_rand_numbers,
+                  bins=100,
+                  kde=True)
+ax.set(xlabel='Random numbers', ylabel='Probability density', title='Probability distribution of random numbers in the range 90-110')
 plt.show()
 
+# Uniform distribution
+list_of_uniform_random_numbers = uniform.rvs(size=10000, loc = 90, scale=20)
+ax = sns.distplot(list_of_uniform_random_numbers,
+                  bins=100,
+                  kde=True,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Uniform Distribution ', ylabel='Frequency')
+plt.show()
+plt.plot(list_of_uniform_random_numbers, uniform.pdf(list_of_uniform_random_numbers))
+plt.show()
