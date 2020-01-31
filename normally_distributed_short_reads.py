09161458826fe1a50095f4e83f0f8562838616de
Soumen Ganguly <soumendotganguly@gmail.com>
2020-01-31 01:26:00 +0100

    Added file which samples short reads from long reads

--- normally_distributed_short_reads.py
+++ normally_distributed_short_reads.py
@@ -3,34 +3,72 @@ from scipy.stats import uniform, norm
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import truncnorm
+import re
+import json
 
-# Normal distribution in the range 90-110
+
+fileLocation = open('data.fastq','r')
+fileContents = fileLocation.read()
+
+fileElements = fileContents.split('\n')
+
+fileLocation.close()
+
+allReadDict = {}
+
+# Store the ID and reads(long) in a dictionary
+
+oneReadDict = {}
+for i in range(len(fileElements)):
+    if re.match(r'^@', fileElements[i]):
+        oneReadDict[fileElements[i]] = []
+        if re.match(r'[A-Z]', fileElements[i+1]):
+            oneReadDict[fileElements[i]].append(fileElements[i+1])
+    else:
+        continue
+
+reads = oneReadDict.values()
+ids = list(oneReadDict.keys())
+
+# Sample short reads from normal distribution of read lengths
+
 def get_truncated_norm(mean=0, sd=1, low=0, upp=10):
     return truncnorm((low-mean)/sd, (upp-mean)/sd, loc=mean, scale=sd)
 
-long_read_length = 12000
-sum = 0
-short_read_length = []
-counter=0
+temp = 0
+for r in reads:
+    longReadLength = len(r[0])
+    normDist = []
+    sum = 0
+    normReads = {}
+    counter = 0
 
-while counter==0:
-    data = get_truncated_norm(mean=95, sd=1, low=90, upp=110).rvs()
-    sum +=int(data)
-    if sum<long_read_length:
-        short_read_length.append(int(data))
-        continue
-    else:
-        sum=sum-int(data)
-        counter=1
+    while counter == 0:
+        normShortReadLen = get_truncated_norm(mean=95, sd=1, low=90, upp=110).rvs()
+        normDist.append(normShortReadLen)
+        if sum+int(normShortReadLen)<longReadLength:
+            normReads[r[0][sum:sum+int(normShortReadLen)]] = sum
+            sum+=int(normShortReadLen)
+            continue
+        else:
+            counter=1
+    allReadDict[ids[temp]] = normReads
+    print(ids[temp])
+    temp+=1
+    print(len(normReads))
+    print(longReadLength-sum)
 
-print(len(short_read_length))
-print(long_read_length-sum)
-print(short_read_length)
+# PLot the distribution of short read lengths for one long read
 
-ax = sns.distplot(short_read_length,
+ax = sns.distplot(normDist,
                   bins=100,
                   kde=True,
                   color='skyblue',
                   hist_kws={"linewidth": 15,'alpha':1})
 ax.set(xlabel='Distribution of short read lengths', ylabel='Frequency')
 plt.show()
+
+# Store the contents into a json file
+
+with open('normallydistributedreads.json','w') as jsonfile:
+    json.dump(allReadDict,jsonfile)
