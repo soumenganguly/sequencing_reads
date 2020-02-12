1a8e5536ebbd9199536bba858b8220746c680484
Soumen Ganguly <soumendotganguly@gmail.com>
2020-02-12 14:58:19 +0100

    Short reads with insert sizes and paired ends

--- normally_distributed_short_reads.py
+++ normally_distributed_short_reads.py
@@ -32,35 +32,46 @@ ids = list(oneReadDict.keys())
 
 # Sample short reads from normal distribution of read lengths
 
-def get_truncated_norm(mean=0, sd=1, low=0, upp=10):
-    return truncnorm((low-mean)/sd, (upp-mean)/sd, loc=mean, scale=sd)
+def get_truncated_norm(m=0, sd=1, low=0, upp=10):
+    return truncnorm((low-m)/sd, (upp-m)/sd, loc=m, scale=sd)
 
-temp = 0
-for r in reads:
-    longReadLength = len(r[0])
-    normDist = []
-    sum = 0
-    normReads = {}
-    counter = 0
 
-    while counter == 0:
-        normShortReadLen = get_truncated_norm(mean=95, sd=1, low=90, upp=110).rvs()
-        normDist.append(normShortReadLen)
-        if sum+int(normShortReadLen)<longReadLength:
-            normReads[r[0][sum:sum+int(normShortReadLen)]] = sum
-            sum+=int(normShortReadLen)
-            continue
-        else:
-            counter=1
-    allReadDict[ids[temp]] = normReads
-    print(ids[temp])
-    temp+=1
-    print(len(normReads))
-    print(longReadLength-sum)
+def sample_short_reads(mean, standard_dev, insertSize):
+    temp = 0
+    for r in reads:
+        longReadLength = len(r[0])
+        normDist = []
+        readList = []
+        counter = 0
+        readCounter = 0
+        boundary = 0
+
+        while counter<=0:
+            normShortReadLen = get_truncated_norm(m=mean, sd=standard_dev, low=90, upp=110).rvs()
+            normDist.append(normShortReadLen)  # Normally distributed short read lengths
+            if (readCounter + 2*int(normShortReadLen) + insertSize) <= longReadLength:
+                r1 = r[0][readCounter:readCounter+int(normShortReadLen)]    # The 0 is used to index the element stored as [[]] in r.
+                r2 = r[0][readCounter + int(normShortReadLen) + insertSize: readCounter + int(normShortReadLen) + insertSize + int(normShortReadLen)]
+                readList.append([r1,r2])
+                boundary = readCounter + int(normShortReadLen) + insertSize + int(normShortReadLen)
+                readCounter += 1
+                continue
+            else:
+                counter=1
+        allReadDict[ids[temp]] = readList
+        print(ids[temp])
+        temp+=1
+        print(len(readList))
+        print(longReadLength, boundary)
+        print(longReadLength-boundary)
+    return allReadDict, normDist
+
+
+readDict, ndist = sample_short_reads(95,1,200)
 
 # PLot the distribution of short read lengths for one long read
 
-ax = sns.distplot(normDist,
+ax = sns.distplot(ndist,
                   bins=100,
                   kde=True,
                   color='skyblue',
@@ -71,4 +82,4 @@ plt.show()
 # Store the contents into a json file
 
 with open('normallydistributedreads.json','w') as jsonfile:
-    json.dump(allReadDict,jsonfile)
+    json.dump(readDict,jsonfile)
